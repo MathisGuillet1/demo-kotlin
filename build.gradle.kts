cc1d74e4e3285756f75f363a3d715dc970e5400d
MathisGuillet1 <mathis.guillet@outlook.com>
2024-08-05 16:16:29 +0200

    Fix Kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("org.springframework.boot") version "3.3.2"
     id("io.spring.dependency-management") version "1.1.6"
     kotlin("jvm") version "2.0.0"
-    kotlin("plugin.spring") version "1.9.24"
+    kotlin("plugin.spring") version "2.0.0"
 }
 
 group = "com.qlik"
@@ -26,9 +26,11 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.amshove.kluent:kluent:1.72")
+
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
